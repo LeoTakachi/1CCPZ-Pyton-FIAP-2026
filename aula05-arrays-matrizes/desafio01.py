cc1264ba70982791e85fa673cca1340d99859a7a
labsfiap <leotakachi27@gmail.com>
2026-05-08 21:26:02 -0300

    novas atividades

---
+++ aula05-arrays-matrizes/desafio01.py
@@ -0,0 +1,12 @@
+lista_nomes = ["Ana", "João", "Leo", "Bia"  ]
+
+for i in range(len(lista_nomes)):
+    for j in range(i+1, len(lista_nomes)):
+        print(lista_nomes[i], "e", lista_nomes[j])
+
+
+
+
+
+
+
